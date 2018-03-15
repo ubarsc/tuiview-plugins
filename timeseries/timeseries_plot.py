7c3e2bf5051cd88e6413db961eaa5349ba42deb6
Sam Gillingham <gillingham.sam@gmail.com>
2018-03-15 16:43:27 +1000

    further development on timeseries plugin

--- timeseries/timeseries_plot.py
+++ timeseries/timeseries_plot.py
@@ -22,20 +22,22 @@ https://bitbucket.org/chchrsc/tuiview/wiki/Plugins
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 from __future__ import print_function, division
+
 import numpy
-from osgeo import ogr
 from tuiview import pluginmanager
 from tuiview import viewerlayers
 from tuiview import vectorrasterizer
 from tuiview import viewerLUT
+from tuiview import plotwidget
 from tuiview.viewerwidget import VIEWER_TOOL_POLYGON, VIEWER_TOOL_NONE
 from tuiview.viewerwidget import VIEWER_TOOL_QUERY
 
-from PyQt5.QtGui import QImage, QPainter
-from PyQt5.QtCore import QObject, QAbstractTableModel, Qt, QPoint
+from PyQt5.QtCore import QObject, Qt, pyqtSignal
 from PyQt5.QtWidgets import QAction, QApplication, QMessageBox, QHBoxLayout
-from PyQt5.QtWidgets import QVBoxLayout, QPushButton, QTableView, QDialog
-from PyQt5.QtWidgets import QLineEdit
+from PyQt5.QtWidgets import QVBoxLayout, QDockWidget, QWidget
+from PyQt5.QtGui import QPen
+
+PLOT_PADDING = 0.2  # of the range of data
 
 def name():
     "Needed by TuiView"
@@ -57,7 +59,76 @@ def action(actioncode, viewer):
         # make sure the object isn't garbage collected
         app = QApplication.instance()
         app.savePluginHandler(handler)
+
+class TimeseriesDockWidget(QDockWidget):
+    """
+    Dockable window that displays the timeseries plot
+    """
+    # signals
+    profileClosed = pyqtSignal(QDockWidget, name='profileClosed')
+
+    def __init__(self, parent):
+        QDockWidget.__init__(self, "Profile", parent)
+
+        # create a new widget that lives in the dock window
+        self.dockWidget = QWidget()
+        self.mainLayout = QVBoxLayout()
+
+        self.plotWidget = plotwidget.PlotLineWidget(self)
+        self.mainLayout.addWidget(self.plotWidget)
+
+        self.whitePen = QPen(Qt.white)
+        self.whitePen.setWidth(1)
+        self.redPen = QPen(Qt.red)
+        self.redPen.setWidth(1)
+        self.greenPen = QPen(Qt.green)
+        self.greenPen.setWidth(1)
+        self.bluePen = QPen(Qt.blue)
+        self.bluePen.setWidth(1)
+
+        self.dockWidget.setLayout(self.mainLayout)
         
+        # tell the dock window this is the widget to display
+        self.setWidget(self.dockWidget)
+
+    def plotData(self, data, steps):
+        """
+        Put the data into our plotWidget
+        """
+        # get rid of curves from last time
+        self.plotWidget.removeCurves()
+
+        if len(data.shape) == 2:
+            # multi band image
+            penList = [self.redPen, self.greenPen, self.bluePen]
+            for band in range(3):
+                bandData = data[..., band]
+
+                #print(steps, bandData)
+                curve = plotwidget.PlotCurve(steps, bandData, penList[band])
+                self.plotWidget.addCurve(curve)
+        else:
+            # single band
+            curve = plotwidget.PlotCurve(steps, data, self.whitePen)
+            self.plotWidget.addCurve(curve)
+
+        # set the Y Range a bit larger than the data
+        minVal = data.min()
+        maxVal = data.max()
+        rangeVal = maxVal - minVal
+        paddingAmount = rangeVal * PLOT_PADDING
+
+        minVal -= paddingAmount
+        maxVal += paddingAmount
+
+        self.plotWidget.setYRange(minVal, maxVal)
+        
+    def closeEvent(self, event):
+        """
+        Window is being closed - inform plugin
+        """
+        self.profileClosed.emit(self)
+
 class TimeseriesPlot(QObject):
     """
     Object that is the plugin. Create actions and menu.
@@ -65,6 +136,7 @@ class TimeseriesPlot(QObject):
     def __init__(self, viewer):
         QObject.__init__(self)
         self.viewer = viewer
+        self.plotWindow = None
 
         # Create actions
         self.pointAct = QAction(self, triggered=self.pointTimeseries)
@@ -72,6 +144,7 @@ class TimeseriesPlot(QObject):
 
         self.polyAct = QAction(self, triggered=self.polyTimeseries)
         self.polyAct.setText("Do timeseries analysis on a polygon")
+        self.polyAct.setEnabled(False)
 
         # Create menu
         tseriesMenu = viewer.menuBar().addMenu("T&imeseries")
@@ -134,6 +207,21 @@ class TimeseriesPlot(QObject):
                 count += 1
 
         print(data, steps)
+        if self.plotWindow is None:
+            self.plotWindow = TimeseriesDockWidget(self.viewer)
+            self.viewer.addDockWidget(Qt.TopDockWidgetArea, self.plotWindow)
+            self.plotWindow.setFloating(True) # detach so it isn't docked by default
+            # this works to prevent it trying to dock when dragging
+            # but double click still works
+            self.plotWindow.setAllowedAreas(Qt.NoDockWidgetArea) 
+
+            # grab the signal the profileDock sends when it is closed
+            self.plotWindow.profileClosed.connect(self.profileClosed)
+
+
+        data = numpy.array(data)
+        steps = numpy.array(steps)
+        self.plotWindow.plotData(data, steps)
 
     def polyTimeseries(self):
         """
@@ -147,3 +235,10 @@ class TimeseriesPlot(QObject):
         """
         # turn off the tool
         self.viewer.viewwidget.setActiveTool(VIEWER_TOOL_NONE, id(self))
+
+    def profileClosed(self, profileDock):
+        """
+        Plot dock window has been closed. Remove our reference 
+        so we open a new one next time.
+        """
+        self.plotWindow = None
